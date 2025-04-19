0f77d680c2e423229a7987d73b8bec64ca9e81d2
MaxD3mage <terzi.kirill@gmail.com>
2025-04-19 21:15:15 +0500

    Done Task

--- run.cs
+++ run.cs
@@ -6,77 +6,91 @@ namespace test_dot_hotel;
 
 class HotelCapacity
 {
-    static bool CheckCapacity(int maxCapacity, List<Guest> guests)
+    private static bool CheckCapacity(int maxCapacity, List<Guest> guests)
     {
-        // TODO: реализация алгоритма
-        return true; // или false
+        var events = new List<Event>();
+        
+        foreach (var guest in guests)
+        {
+            var checkIn = DateTime.Parse(guest.CheckIn);
+            var checkOut = DateTime.Parse(guest.CheckOut);
+            
+            events.Add(new Event { Date = checkIn, Change = 1 });
+            events.Add(new Event { Date = checkOut, Change = -1 });
+        }
+
+        events.Sort((a, b) => 
+        {
+            var dateCompare = a.Date.CompareTo(b.Date);
+            return dateCompare != 0 ? dateCompare : a.Change.CompareTo(b.Change);
+        });
+        
+        var currentCapacity = 0;
+        
+        foreach (var evt in events)
+        {
+            currentCapacity += evt.Change;
+
+            if (currentCapacity > maxCapacity)
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
-
-
-    class Guest
+    private class Event
+    {
+        public DateTime Date { get; set; }
+        public int Change { get; set; }
+    }
+    
+    private class Guest
     {
         public string Name { get; set; }
         public string CheckIn { get; set; }
         public string CheckOut { get; set; }
     }
 
-
-    static void Main()
+    public static void Main()
     {
-        int maxCapacity = int.Parse(Console.ReadLine());
-        int n = int.Parse(Console.ReadLine());
+        var maxCapacity = int.Parse(Console.ReadLine());
+        var n = int.Parse(Console.ReadLine());
 
-
-        List<Guest> guests = new List<Guest>();
-
-
-        for (int i = 0; i < n; i++)
+        var guests = new List<Guest>();
+        
+        for (var i = 0; i < n; i++)
         {
-            string line = Console.ReadLine();
-            Guest guest = ParseGuest(line);
+            var line = Console.ReadLine();
+            var guest = ParseGuest(line);
             guests.Add(guest);
         }
 
-
-        bool result = CheckCapacity(maxCapacity, guests);
-
+        var result = CheckCapacity(maxCapacity, guests);
 
         Console.WriteLine(result ? "True" : "False");
     }
 
-
     // Простой парсер JSON-строки для объекта Guest
-    static Guest ParseGuest(string json)
+    private static Guest ParseGuest(string json)
     {
         var guest = new Guest();
 
-
         // Извлекаем имя
-        Match nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
+        var nameMatch = Regex.Match(json, "\"name\"\\s*:\\s*\"([^\"]+)\"");
         if (nameMatch.Success)
             guest.Name = nameMatch.Groups[1].Value;
-
-
+        
         // Извлекаем дату заезда
-        Match checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
+        var checkInMatch = Regex.Match(json, "\"check-in\"\\s*:\\s*\"([^\"]+)\"");
         if (checkInMatch.Success)
             guest.CheckIn = checkInMatch.Groups[1].Value;
 
-
         // Извлекаем дату выезда
-        Match checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
+        var checkOutMatch = Regex.Match(json, "\"check-out\"\\s*:\\s*\"([^\"]+)\"");
         if (checkOutMatch.Success)
             guest.CheckOut = checkOutMatch.Groups[1].Value;
 
-
         return guest;
-    }
-}
-
-public class HotelTask
-{
-    public static void Main()
-    {
-        
     }
 }
